2bc626e9bc98e6ac31add4aeeff4ca534fbabeba
Giuseppe Cerati <cerati@fnal.gov>
2019-03-25 10:31:31 -0500

    add tests in many flavors...

--- xtensor-basic-test.cc
+++ xtensor-basic-test.cc
@@ -2,6 +2,15 @@
 #include "xtensor/xarray.hpp"
 #include "xtensor/xio.hpp"
 #include "xtensor/xview.hpp"
+#include "xtensor/xrandom.hpp"
+
+#include "xtensor/xvectorize.hpp"
+#include "xtensor/xfixed.hpp"
+
+int f(int a, int b)
+{
+  return a + 2 * b;
+}
 
 int main(int argc, char* argv[])
 {
@@ -34,7 +43,7 @@ int main(int argc, char* argv[])
     std::cout << xt::view(arr4, xt::range(0,3)) << std::endl;
 
     // warning: pow seem not to work with xsimd
-    res = xt::pow(arr3, xt::view(arr4, xt::range(0,3)));
+    // res = xt::pow(arr3, xt::view(arr4, xt::range(0,3)));
 
     std::cout << res << std::endl;
 
@@ -42,9 +51,56 @@ int main(int argc, char* argv[])
 
     std::cout << arr4 << std::endl;
 
-    res = xt::pow(arr3, arr4);
+    // res = xt::pow(arr3, arr4);
 
     std::cout << res << std::endl;
+
+    xt::xtensor<float, 2> tsr1
+      {{1.0, 2.0, 3.0},
+       {4.0, 5.0, 6.0}};
+
+    for (auto& el : tsr1.shape()) {std::cout << el << ", "; }
+    std::cout << std::endl;
+    std::cout << tsr1 << std::endl;
+    std::cout << tsr1(0,1) << std::endl;
+    std::cout << tsr1(1,2) << std::endl;
+    std::cout << xt::view(tsr1,xt::all(),1) << std::endl;
+
+    xt::xarray<float> tsr2
+      {{1.0, 2.0, 3.0},
+       {4.0, 5.0, 6.0}};
+
+    for (auto& el : tsr2.shape()) {std::cout << el << ", "; }
+    std::cout << std::endl;
+    std::cout << tsr2 << std::endl;
+    std::cout << tsr2(0,1) << std::endl;
+    std::cout << tsr2(1,2) << std::endl;
+    std::cout << xt::view(tsr2,xt::all(),1) << std::endl;
+
+    std::vector<size_t> shape = {10, 3};
+    xt::xarray<float> tsr3(shape);
+    // xt::view(tsr3,0,xt::all()) = xt::xarray<float>({1.0, 2.0, 3.0});
+    // xt::view(tsr3,1,xt::all()) = xt::xarray<float>({4.0, 5.0, 6.0});
+    float count = 0;
+    for (size_t i=0;i<3;++i) xt::view(tsr3,xt::all(),i) = xt::random::randn<float>({10});
+
+    for (auto& el : tsr3.shape()) {std::cout << el << ", "; }
+    std::cout << std::endl;
+    std::cout << tsr3 << std::endl;
+    std::cout << tsr3(0,1) << std::endl;
+    std::cout << tsr3(1,2) << std::endl;
+    std::cout << xt::view(tsr3,xt::all(),1) << std::endl;
+
+    auto vecf = xt::vectorize(f);
+    xt::xarray<int> a = { 11, 12, 13 };
+    xt::xarray<int> b = {  1,  2,  3 };
+    xt::xarray<int> res_f = vecf(a, b);
+    std::cout << "res_f\n" << res_f << std::endl;
+
+    xt::xarray<int> arra{{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+    xt::xarray<int> arrb{{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+    xt::xarray<int> res_af = vecf(xt::view(arra,0,xt::all()), xt::view(arrb,0,xt::all()));
+    std::cout << res_af << std::endl;
 
     return 0;
 }
--- xtensor-test.cc
+++ xtensor-test.cc
@@ -9,23 +9,50 @@
 
 // /usr/local/Cellar/gcc/7.1.0/bin/c++-7 -O2 -mavx -DXTENSOR_USE_XSIMD -I /Users/cerati/miniconda3/include/ xtensor-test.cc -o xtensor-test.exe
 
-int main(int argc, char* argv[])
-{
-  constexpr size_t NN = 10000000;
+void test_v0(size_t NN, std::array<xt::xarray<float>, 36>& input, int type) {
 
   MatriplXT66 A;
-  for (size_t i=0;i<36;++i) A[i] = xt::random::randn<float>({NN});
-  // for (size_t i=0;i<36;++i) A[i] = xt::ones<float>({NN});
+  for (size_t i=0;i<36;++i) A[i] = input[i];
 
   MatriplXT66 B;
-  for (size_t i=0;i<36;++i) B[i] = xt::random::randn<float>({NN});
-  // for (size_t i=0;i<36;++i) B[i] = xt::ones<float>({NN});
+  for (size_t i=0;i<36;++i) B[i] = input[i];
 
   const clock_t begin = clock();
-
   MatriplXT66 C;
-  MultiplyXT66(A, B, C);
+  if (type==1) MultiplyXT66Loop(A, B, C);
+  else if (type==2) MultiplyXT66LoopTile(A, B, C);
+  else MultiplyXT66(A, B, C);
+  const clock_t end = clock();
 
+  for (size_t nn=0; nn<1/*NN*/; nn++) {
+    // std::cout << "nn=" << nn << std::endl;
+    // std::cout << "A" << std::endl;
+    // A.print(std::cout,nn);
+    // std::cout << "B" << std::endl;
+    // B.print(std::cout,nn);
+    std::cout << "C" << std::endl;
+    C.print(std::cout,nn);
+  }
+
+  float time = float(end-begin)/CLOCKS_PER_SEC;
+  if (type==1) std::cout << "v0 -- time for NN=" << NN << " multiplications is " << time << " s (loop version), i.e. per track [s]=" << time/float(NN) << std::endl;
+  else if (type==2) std::cout << "v0 -- time for NN=" << NN << " multiplications is " << time << " s (loop-tile version), i.e. per track [s]=" << time/float(NN) << std::endl;
+  else std::cout << "v0 -- time for NN=" << NN << " multiplications is " << time << " s, i.e. per track [s]=" << time/float(NN) << std::endl;
+}
+
+void test_v1(size_t NN, std::array<xt::xarray<float>, 36>& input, int type) {
+
+  MatriplXT66_v1 A(NN);
+  for (size_t i=0;i<36;++i) A[i] = input[i];
+
+  MatriplXT66_v1 B(NN);
+  for (size_t i=0;i<36;++i) B[i] = input[i];
+
+  const clock_t begin = clock();
+  MatriplXT66_v1 C(NN);
+  if (type==1) MultiplyXT66Loop(A, B, C);
+  else if (type==2) MultiplyXT66Stack(A, B, C);
+  else MultiplyXT66(A, B, C);
   const clock_t end = clock();
 
   for (size_t nn=0; nn<1/*NN*/; nn++) {
@@ -38,7 +65,65 @@ int main(int argc, char* argv[])
     C.print(std::cout,nn);
   }
 
-  std::cout << "time for NN=" << NN << " multiplications is " << float(end-begin)/CLOCKS_PER_SEC << " s" << std::endl;
+  float time = float(end-begin)/CLOCKS_PER_SEC;
+  if (type==1) std::cout << "v1 -- time for NN=" << NN << " multiplications is " << time << " s (loop version), i.e. per track [s]=" << time/float(NN) << std::endl;
+  else if (type==2) std::cout << "v1 -- time for NN=" << NN << " multiplications is " << time << " s (stack version), i.e. per track [s]=" << time/float(NN) << std::endl;
+  else std::cout << "v1 -- time for NN=" << NN << " multiplications is " << time << " s, i.e. per track [s]=" << time/float(NN) << std::endl;
+}
+
+void test_v2(size_t NN, std::array<xt::xarray<float>, 36>& input, int type) {
+
+  MatriplXT66_v2 A(NN);
+  for (size_t i=0;i<36;++i) A[i] = input[i];
+
+  MatriplXT66_v2 B(NN);
+  for (size_t i=0;i<36;++i) B[i] = input[i];
+
+  const clock_t begin = clock();
+  MatriplXT66_v2 C(NN);
+  if (type==1) MultiplyXT66Loop(A, B, C);
+  else if (type==2) MultiplyXT66Stack(A, B, C);
+  else MultiplyXT66(A, B, C);
+  const clock_t end = clock();
+
+  for (size_t nn=0; nn<1/*NN*/; nn++) {
+    // std::cout << "nn=" << nn << std::endl;
+    // std::cout << "A" << std::endl;
+    // A.print(std::cout,nn);
+    // std::cout << "B" << std::endl;
+    // B.print(std::cout,nn);
+    std::cout << "C" << std::endl;
+    C.print(std::cout,nn);
+  }
+
+  float time = float(end-begin)/CLOCKS_PER_SEC;
+  if (type==1) std::cout << "v2 -- time for NN=" << NN << " multiplications is " << time << " s (loop version), i.e. per track [s]=" << time/float(NN) << std::endl;
+  else if (type==2) std::cout << "v2 -- time for NN=" << NN << " multiplications is " << time << " s (stack version), i.e. per track [s]=" << time/float(NN) << std::endl;
+  else std::cout << "v2 -- time for NN=" << NN << " multiplications is " << time << " s, i.e. per track [s]=" << time/float(NN) << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+  constexpr size_t NN = 10000000;
+
+  std::array<xt::xarray<float>, 36> input;
+  for (size_t i=0;i<36;++i) input[i] = xt::random::randn<float>({NN});
+  //for (size_t i=0;i<36;++i) input[i] = xt::linspace<float>(1.,100.,NN);fixme
+
+  std::cout << "done preparing input" << std::endl;
+
+  test_v0(NN,input,0);
+  test_v0(NN,input,0);// do it twice to let it warm up...
+  test_v0(NN,input,1);
+  test_v0(NN,input,2);
+  std::cout << std::endl;
+  test_v1(NN,input,0);
+  test_v1(NN,input,1);
+  test_v1(NN,input,2);
+  std::cout << std::endl;
+  test_v2(NN,input,0);
+  test_v2(NN,input,1);
+  test_v2(NN,input,2);
 
   return 0;
 }
